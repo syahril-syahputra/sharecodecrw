45df09866ff821b84719b0bf29053de0ea6754e1
syahril syahputra <syahril.s@radx.sg>
2024-06-05 10:31:08 +0800

    fixing some bug

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,17 +1,19 @@
 'use client';
-import React from 'react';
+import React, { Suspense } from 'react';
 import { Next13ProgressBar } from 'next13-progressbar';
 
 const Providers = ({ children }: { children: React.ReactNode }) => {
     return (
         <>
-            {children}
-            <Next13ProgressBar
-                height="4px"
-                color="#ff6969"
-                options={{ showSpinner: false }}
-                showOnShallow
-            />
+            <Suspense>
+                {children}
+                <Next13ProgressBar
+                    height="4px"
+                    color="#ff6969"
+                    options={{ showSpinner: false }}
+                    showOnShallow
+                />
+            </Suspense>
         </>
     );
 };
