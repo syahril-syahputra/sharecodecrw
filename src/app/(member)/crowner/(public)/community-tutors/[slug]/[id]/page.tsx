3786aba532a8fca095481d554691bf17da184eb6
KdkChy <kdkchy@venatronics.com>
2024-07-01 16:21:19 +0800

    ref: dir route

---
+++ src/app/(member)/crowner/(public)/community-tutors/[slug]/[id]/page.tsx
@@ -0,0 +1,8 @@
+"use client";
+import { useParams } from "next/navigation";
+
+export default function Page() {
+    const {id} = useParams();
+    console.log(id)
+    return <>Detail with slug</>;
+}
